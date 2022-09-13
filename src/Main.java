61754eb4b2c04a7a26c78e3a848e5bf07f9c6083
MangledMan <Themangle906@gmail.com>
2022-09-13 09:37:02 -0500

    Lab time!

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte byteOne = 127;
+        short shortOne = 32767;
+        int intOne = 2147483647;
+        long longOne = 2147483647;
+        float floatOne = 39.627F;
+        double doubleOne = 847956.736485903;
+        char charOne = 'A';
+        boolean booleanOne = true;
+
+
+        System.out.println("/////////////////////////////////");
+        System.out.println("*Some Person            07/18/22*");
+        System.out.println("*                               *");
+        System.out.println("*        integer types          *");
+        System.out.println("*                               *");
+        System.out.println(" *8 bit - byteOne = "+byteOne+"\t\t*");
+        System.out.println("* 16 bit = " + shortOne + " *");
+        System.out.println("* 32 bit = " + intOne + " *");
+        System.out.println("* 32 bit = " + longOne + " *");
+        System.out.println("* 32 bit = " + floatOne + " *");
+        System.out.println("* 64 bit = " + doubleOne + " *");
+        System.out.println("* 16 bit = " + charOne + " *");
+        System.out.println("* True or False = " + booleanOne + " *");
+    }
+}
